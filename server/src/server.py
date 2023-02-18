fea94354a0462cbc66604d57b425837e733dfefc
mickey li <mickeyhli@outlook.com>
2023-02-18 11:16:53 +0000

    initial flask server

---
+++ server/src/server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Create a URL route in our application for "/"
+@app.route('/participants', methods = ['GET', 'POST', 'DELETE'])
+def participants(user_id):
+    if request.method == 'GET':
+	    """return current set of participants"""
+        # userId = request.args.get('user_id')
+    if request.method == 'POST':
+	    """Add a new participant"""
+        # data = request.form # a multidict containing POST data
+        pass
+    if request.method == 'DELETE':
+	    """delete participant"""
+        # userId = request.args.get('user_id')
+        pass
+    else:
+        pass
+
+# Create a URL route in our application for "/"
+@app.route("/state", methods = ['GET', 'POST'])
+def participants(user_id):
+    if request.method == 'GET':
+	    """return current state of the system"""
+        # userId = request.args.get('user_id')
+    else:
+        pass
+
+# Create a URL route in our application for "/"
+@app.route("/move/<user_id>", methods = ['GET', 'POST'])
+def participants(user_id):
+    if request.method == 'GET':
+	    """return current state of the system"""
+        # userId = request.args.get('user_id')
+    else:
+        pass
+
+
+app.run(port=5000)
