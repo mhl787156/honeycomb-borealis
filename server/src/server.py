e46229cb43b2b04122873ddc57ae8660fc36f8cc
mickey li <mickeyhli@outlook.com>
2023-02-18 11:19:28 +0000

    Fixed indentation issues

--- server/src/server.py
+++ server/src/server.py
@@ -4,35 +4,38 @@ app = Flask(__name__)
 
 # Create a URL route in our application for "/"
 @app.route('/participants', methods = ['GET', 'POST', 'DELETE'])
-def participants(user_id):
+def participants():
     if request.method == 'GET':
-	    """return current set of participants"""
+        """return current set of participants"""
         # userId = request.args.get('user_id')
+        pass
     if request.method == 'POST':
-	    """Add a new participant"""
+        """Add a new participant"""
         # data = request.form # a multidict containing POST data
         pass
     if request.method == 'DELETE':
-	    """delete participant"""
-        # userId = request.args.get('user_id')
+        """delete participant"""
+        # userId = request.args.get('user_id')      
         pass
     else:
         pass
 
 # Create a URL route in our application for "/"
 @app.route("/state", methods = ['GET', 'POST'])
-def participants(user_id):
+def state():
     if request.method == 'GET':
-	    """return current state of the system"""
+        """return current state of the system"""
         # userId = request.args.get('user_id')
+        pass
     else:
         pass
 
 # Create a URL route in our application for "/"
 @app.route("/move/<user_id>", methods = ['GET', 'POST'])
-def participants(user_id):
+def move(user_id):
     if request.method == 'GET':
-	    """return current state of the system"""
+        """return current state of the system"""
+        pass
         # userId = request.args.get('user_id')
     else:
         pass
